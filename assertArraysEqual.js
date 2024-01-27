1c184e690f1103b060e12f4bb38112064335dad9
Kim O'Dell <kimbo.odell@gmail.com>
2024-01-27 11:22:33 -0500

    added middle.js function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,26 +1,26 @@
 function eqArrays(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) {
-          return false;
-        } 
-      } 
-      return true;
-    } else {
-      return false;
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
+    return true;
+  } else {
+    return false;
   }
+}
 
-  const assertArraysEqual = function(arr1, arr2) {
-    const arraysAreEqual = eqArrays(arr1, arr2);
-    if (arraysAreEqual) {
-      console.log(`❤️❤️❤️Assertion Passed: [${arr1}] === [${arr2}]`);
-    } else {
-      console.log(`😒😒😒Assertion Failed: [${arr1}] !== [${arr2}]`);
-    }
-  };
+const assertArraysEqual = function(arr1, arr2) {
+  const arraysAreEqual = eqArrays(arr1, arr2);
+  if (arraysAreEqual) {
+    console.log(`❤️❤️❤️Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
 
-assertArraysEqual([1,2,3], [1,2,3]);
-assertArraysEqual([1,2], [1,2,3]);
-assertArraysEqual([1,2,3], [1,2,3,4]);
-assertArraysEqual([1,2,3], [1,23]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [1, 23]);
---
+++ middle.js
@@ -0,0 +1,40 @@
+function eqArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  const arraysAreEqual = eqArrays(arr1, arr2);
+  if (arraysAreEqual) {
+    console.log(`❤️❤️❤️Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+function middle(array) {
+  let newArray = [];
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 !== 0) {
+    newArray.push(array[((array.length - 1) / 2)]); //push middle element to newArray
+  } else {
+    newArray.push(array[(array.length / 2) - 1], array[array.length / 2]);//push middle 2 elements to newArray
+  }
+  return newArray;
+}
+
+assertArraysEqual(middle([1]), []); // => [] 
+assertArraysEqual(middle([1, 2]), []); // => []
+assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);// => [3, 4]
