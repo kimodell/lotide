87fb64a84afdb1129e89fea4d41bbd8aa0e3e5c1
Kim O'Dell <kimbo.odell@gmail.com>
2024-01-27 06:19:27 -0500

    correct based off ai feedback

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,7 +16,7 @@ function eqArrays(arr1, arr2) {
     if (arraysAreEqual) {
       console.log(`❤️❤️❤️Assertion Passed: [${arr1}] === [${arr2}]`);
     } else {
-      console.log(`😒😒😒Assertion Failed: [${arr1} !== [${arr2}]`);
+      console.log(`😒😒😒Assertion Failed: [${arr1}] !== [${arr2}]`);
     }
   };
 
