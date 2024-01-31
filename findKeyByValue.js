394a39ec35264a3fc1c1fd4a627edc0debcd4924
Kim O'Dell <kimbo.odell@gmail.com>
2024-01-30 19:02:14 -0500

    findKeyByValue function added

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`❤️❤️❤️Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(genres, show) {
+  let result = undefined;
+  for (const genre in genres) {
+    if (show === genres[genre]) {
+      result = genre;
+    }
+  }
+  return result;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
