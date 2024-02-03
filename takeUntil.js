da8f2f6f1929bacf5df2656500c3a561e548777d
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-03 12:18:05 -0500

    implement takeUntil function to stop pushing item to a new array until the callback returns truthy

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+const takeUntil = function(array, callback) {  //the callback will be the condition when we need to stop the loop
+  const results = [];
+  for (item of array) {
+    if (callback(item) === false) {  //if the item, when compared to the callback, returns false we cane push it!
+      results.push(item);
+    } else { //but we need to stop the loop as soon as the callback returns true for the item!
+      break;
+    }
+  }
+  return results;
+};
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(arr1, arr2) {
+  const arraysAreEqual = eqArrays(arr1, arr2);
+  if (arraysAreEqual) {
+    console.log(`❤️❤️❤️Assertion Passed: [${arr1}] === [${arr2}]`);
+  } else {
+    console.log(`😒😒😒Assertion Failed: [${arr1}] !== [${arr2}]`);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2,]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, ["I've", 'been', 'to', 'Hollywood']);
