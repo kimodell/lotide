cc098474553553de16fb258a4e504cf18f058338
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-13 18:03:04 -0500

    fixed tail function to ensure result variable is delacred in the test code

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,5 +2,5 @@ const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-//tail(words); 
-assertEqual(words.length, 3);
+const result = tail(words); 
+assertEqual(result.length, 2);
