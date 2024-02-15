9f3c7dbf3e9746199bb4fc0904b901940ad0025d
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-14 19:21:45 -0500

    converted pervious test cases to mocha tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //should pass
-assertEqual(eqArrays([1, 2], [1, 2, 3]), true); //should fail
-assertEqual(eqArrays([1, 2, 3], [1, 23]), true);  //should fail
+//assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //should pass
+//assertEqual(eqArrays([1, 2], [1, 2, 3]), true); //should fail
+//assertEqual(eqArrays([1, 2, 3], [1, 23]), true);  //should fail
+
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for [1, 2], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2], [1, 2, 3]), false);
+  });
+  it("returns false for [1, 2, 3], [1, 23]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 23]), false);
+  });
+  it("returns true for [], []", () => {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,8 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+//const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
+/*const words = ["Yo Yo", "Lighthouse", "Labs"];
 const result = tail(words);
 assertEqual(result.length, 2);
 
@@ -11,4 +12,23 @@ assertEqual(result2.length, 3);
 
 const words3 = ["one", "year"] ;
 const result3 = tail(words3);
-assertEqual(result3.length, 1);
+assertEqual(result3.length, 1);*/
+
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Yo Yo', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns ['soup', 'for', 'you'] for ['no', 'soup', 'for', 'you']", () => {
+    assert.deepEqual(tail(['no', 'soup', 'for', 'you']), ['soup', 'for', 'you']);
+  });
+  it("returns [2, 3, 4] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("returns [] for ['one']", () => {
+    assert.deepEqual(tail(['one']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
+
