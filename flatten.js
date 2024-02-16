5fda9bdfcfd652021d5c844fade9a71f36b333fa
Kim O'Dell <kimbo.odell@gmail.com>
2024-02-16 18:40:12 -0500

    added addtional tests and comments to flatten.js

--- flatten.js
+++ flatten.js
@@ -20,16 +20,24 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
+//create a function that takes an array with nested arrays and returns a new array with all the elements, but not nested arrays
 function flatten(array) {
+  //first define your new, empty array
   let newArray = [];
+  //loop through ther array with a forloop
   for (let i = 0; i < array.length; i++) {
+    //use Array.isArry to seeif the element [i] of the main array is also an array
     if (Array.isArray(array[i])) {
+      //if it is a nested array, do a new for loop to get the elements of the nested array
       for (let j = 0; j < array[i].length; j++) {
+        //and push the elements of the nested array to the newArray
         newArray.push(array[i][j]);
       }
+      //else, if the element[i] is not a nested array, just push it to the nmew array
     } else {
       newArray.push(array[i]);
     }
+    //then return the array!
   } return newArray;
 }
 
@@ -39,5 +47,9 @@ flatten([1, 2, [3, 4], 5, [6], 7, 8]);
 flatten(['bears', ['beets', 'Battlestar Galactica']]);
 
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, 3, 4, 5, 6]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6], 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8]);
+assertArraysEqual(flatten(['bears', ['beets', 'Battlestar Galactica']]), ['bears', 'beets', 'Battlestar Galactica']);
 
 module.exports = flatten;
+//A function that takes an array containing nested arrays and returns a new array with all elements of the original array, without any nested arrays.
